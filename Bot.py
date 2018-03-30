c76ac8cd9c6fa78dfd5ec085e8e83c7055292b73
Alexander Weitz <ajveits@gmail.com>
2018-03-30 21:49:33 +0300

    Implemented fetching of data from DB

--- Bot.py
+++ Bot.py
@@ -5,11 +5,11 @@ import os
 
 class Bot:
 
-    DONT_UNDERSTAND = ["lol, not sure what you mean","wut","i don't understand"]
     HINT_GREET = os.path.join("lib","hints","greet.lsv")
+    PRECISION = 100
 
-    def __init__(self):
-        self.dont_understand = self.DONT_UNDERSTAND
+    def __init__(self, precision = PRECISION):
+        self.precision  = precision
         self.greeted = False
         self.db = DbAccess.DbAccess()
         return
@@ -18,21 +18,41 @@ class Bot:
         if(not self.greeted and self.isGreeting(str)):
             self.greeted = True
             return self.greet()
-        answer = self.find_answer(str)
+        answer = self.findAnswer(str)
         if answer is None:
-            return random.choice(self.dont_understand)
+            return random.choice(self.db.getUnknowns(self.precision))
         return answer
 
     def greet(self):
-        return "hi"
+        precision = self.precision
+        return random.choice(self.db.getGreetings(precision))
 
+    # Gets words from hint file and db, and tries to see if the greeting
     def isGreeting(self, str):
+        precision = self.precision
         with open(self.HINT_GREET) as f:
             greetHints = f.readlines()
         greetHints = [x.strip() for x in greetHints]
+        greetHints = greetHints + self.db.getGreetings(precision)
+        if matches(str.split(" "), greetHints):
+            return True
         return False
 
 
-    def find_answer(self, str):
+    def findAnswer(self, str):
         return None
+
+
+# Helper functions
+def inArr(str,arr):
+    for s in arr:
+        if str.lower() == s.lower():
+            return True
+    return False
+def matches(arr1,arr2):
+    for s1 in arr1:
+        for s2 in arr2:
+            if s1.lower() == s2.lower():
+                return True
+    return False
 
--- lib/DbAccess.py
+++ lib/DbAccess.py
@@ -5,8 +5,41 @@ class DbAccess:
 
     DB_CONFIG = "database.ini"
 
+    GREETINGS_TABLE = "greetings"
+    GREETINGS_COLUMNS = {"name":"gname","rating":"rating"}
+    UNKNOWN_TABLE = "unknown"
+    UNKNOWN_COLUMNS = {"name":"uname","rating":"rating"}
+
+    TABLE_UNKNOWN = ""
+
     def __init__(self):
         self.config = Configurator.Configurator()
         self.dbConfig = self.config.dbConfig(self.DB_CONFIG)
         #connecting to DB
         self.conn = psycopg2.connect(host=self.dbConfig["host"],database=self.dbConfig["database"], user=self.dbConfig["user"], password=self.dbConfig["password"])
+
+    def getGreetings(self, precision):
+        t = self.GREETINGS_TABLE
+        c = self.GREETINGS_COLUMNS
+        return self.getTopAnswers(precision, t, c['name'], c['rating'])
+
+    def getUnknowns(self, precision):
+        t = self.UNKNOWN_TABLE
+        c = self.UNKNOWN_COLUMNS
+        return self.getTopAnswers(precision, t, c['name'], c['rating'])
+
+    # method for the most common way to fetch data
+    def getTopAnswers(self, precision, table, cname, crating):
+        cur = self.conn.cursor()
+        query = "SELECT {} FROM {} ORDER BY {} LIMIT {}".format(cname, table, crating, precision)
+        cur.execute(query)
+        result = tuplesToArr(cur.fetchall())
+        cur.close()
+        return result
+
+# Helper functions
+def tuplesToArr(tuples):
+    result = []
+    for tuple in tuples:
+        result.append(list(tuple)[0])
+    return result
